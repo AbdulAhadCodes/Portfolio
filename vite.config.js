eb7b88ce14fa987674dd6925f43e6e1df22e4806
Abdul Ahad <126808865+AbdulAhadCodes@users.noreply.github.com>
2024-03-30 22:22:32 +0500

    Update 3

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    assetsInlineLimit: 0,
-    rollupOptions: {
-      plugins: [
-        {
-          name: 'pdf-file-loader',
-          transform(src, id) {
-            if (id.endsWith('.pdf')) {
-              const base64 = src.toString('base64');
-              return `export default "data:application/pdf;base64,${base64}"`;
-            }
-          },
-        },
-      ],
-    },
-  },
-});
+})
